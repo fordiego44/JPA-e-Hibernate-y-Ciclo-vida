719f8d94d229a89b4cbc7a9ef2da89c65d92158f
Diego Armando <fordiego44@gmail.com>
2025-04-12 06:05:24 -0500

    JPA-e-Hibernate-y-Ciclo-vida

---
+++ src/test/java/com/diego/curso/springboot/jpa/apringboot_jpa/ApringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.curso.springboot.jpa.apringboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
